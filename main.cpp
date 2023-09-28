619eed5ba04510b960e32d97860d89e9e196e36d
Mantas Laurutis <144427355+Mcitas@users.noreply.github.com>
2023-09-28 19:08:20 +0300

    Add files via upload
    1. Duomenu ivedimas

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iomanip>
-
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
 using std::vector;
-using std::setw;
-using std::setprecision;
-using std::fixed;
-using std::left;
-using std::right;
 
 struct Studentas {
 string vardas, pavarde;
 vector <int> pazymys;
-float egzaminas;
-float vidurkis;
+int egzaminas;
 };
 
 int main()
@@ -26,7 +15,6 @@ int main()
     Studentas x;
     vector <Studentas> kursas;
     int m, n;
-    float sum = 0;
     cout << "Iveskite studentu skaiciu kurse:" << endl;
     cin >> m;
     for (int i = 0; i < m; i++)
@@ -43,22 +31,11 @@ int main()
             cout << "Iveskite " << j + 1 << " pazymi:" << endl;
             cin >> p;
             x.pazymys.push_back(p);
-            sum += p;
-
         }
         cout << "Iveskite egzamino rezultata:" << endl;
         cin >> x.egzaminas;
-        x.vidurkis = sum/n;
         kursas.push_back(x);
         x.pazymys.clear();
-        sum = 0;
-    }
-
-    cout << setw(15) << left << "Vardas" << setw(15) << right << "Pavarde" << setw(20) << right << "Galutinis (Vid.)" << endl;
-    cout << "----------------------------------------------------" << endl;
-    for (auto &a: kursas)
-    {
-        cout << setw(15) << left << a.vardas << setw(15) << right << a.pavarde << setw(20) << right << setprecision(2) << fixed << a.vidurkis*0.4 + a.egzaminas*0.6 << endl;
     }
     return 0;
 }
