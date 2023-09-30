58190715cd4c58684362c3ff67f43f525bd29998
Mantas Laurutis <144427355+Mcitas@users.noreply.github.com>
2023-09-30 18:26:54 +0300

    Add files via upload
    6. 1) Prideta failo skaitymo funkcija

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <time.h>
 #include <cstdlib>
+#include <fstream>
 
 using std::cout;
 using std::cin;
@@ -19,6 +20,9 @@ using std::right;
 using std::end;
 using std::begin;
 using std::sort;
+using std::ifstream;
+using std::ofstream;
+using std::cerr;
 
 struct Studentas {
 string vardas, pavarde;
@@ -27,6 +31,52 @@ float egzaminas;
 float vidurkis;
 float mediana;
 };
+
+void Skaityk (int &m, Studentas x, vector <Studentas> &kursas)
+{
+    ifstream fd ("kursiokai.txt");
+
+    int paz, sum = 0, n = 0;
+    fd.ignore(80, '\n');
+
+    while (fd >> x.vardas >> x.pavarde)
+    {
+        cout << x.vardas << " " << x.pavarde << endl;
+        cout << "Pazymiai:" << endl;
+        for (int i = 0; i < m; i++)
+        {
+            fd >> paz;
+            x.pazymys.push_back(paz);
+            n++;
+            sum += paz;
+            cout << paz << " ";
+        }
+        fd >> x.egzaminas;
+
+        cout << x.egzaminas;
+
+        fd.ignore(80, '\n');
+        x.vidurkis = static_cast<float>(sum)/n;
+        //----------------------skaiciuojam mediana---------------------------
+        sort(x.pazymys.begin(), x.pazymys.end());
+        int dyd = x.pazymys.size();
+        if (dyd % 2 == 0)
+        {
+            x.mediana = static_cast<float>(x.pazymys[(dyd - 1) / 2] + x.pazymys[dyd / 2]) / 2;
+        }
+        else
+        {
+        x.mediana = x.pazymys[dyd / 2];
+        }
+        //---------------------------------------------------------------------
+        kursas.push_back(x);
+        x.pazymys.clear();
+        sum = 0;
+        n = 0;
+        cout << endl;
+    }
+    fd.close();
+}
 
 void generuok (Studentas &x, int &n, float &sum)
 {
@@ -50,70 +100,84 @@ int main()
 {
     Studentas x;
     vector <Studentas> kursas;
-    int m, n = 0, y;
+    int m = 0, n = 0, y;
     float sum = 0;
-    cout << "Iveskite studentu skaiciu kurse:" << endl;
-    cin >> m;
-    for (int i = 0; i < m; i++)
+
+    cout << "Kaip ivesite duomenis? (1 - Ranka; 2 - Is failo)" << endl;
+    cin >> y;
+
+    if (y == 1)
     {
-        cout << "Iveskite " << i + 1 << " studento varda:" << endl;
-        cin >> x.vardas;
-        cout << "Iveskite " << i + 1 << " studento pavarde:" << endl;
-        cin >> x.pavarde;
-
-        cout << "Ar siam studentui generuoti pazymius? (1 - TAIP, 2 - NE)" << endl;
-        cin >> y;
-        if (y == 1)
+        cout << "Iveskite studentu skaiciu kurse:" << endl;
+        cin >> m;
+        for (int i = 0; i < m; i++)
         {
-           generuok(x, n, sum);
-        }
-        else
-        {
-            cout << "Iveskite " << i + 1 << " studento pazymius:" << endl;
+            cout << "Iveskite " << i + 1 << " studento varda:" << endl;
+            cin >> x.vardas;
+            cout << "Iveskite " << i + 1 << " studento pavarde:" << endl;
+            cin >> x.pavarde;
 
-        //----------------pazymiu ivedimas, kur breaking point - 2 enter paspaudimai-------------------
-            while (true)
+            cout << "Ar siam studentui generuoti pazymius? (1 - TAIP, 2 - NE)" << endl;
+            cin >> y;
+            if (y == 1)
             {
-                string input;
-                getline(cin, input);
-                if (input.empty())
+               generuok(x, n, sum);
+            }
+            else
+            {
+                cout << "Iveskite " << i + 1 << " studento pazymius:" << endl;
+
+            //----------------pazymiu ivedimas, kur breaking point - 2 enter paspaudimai-------------------
+                while (true)
                 {
-                    if (x.pazymys.size() >= 1)
+                    string input;
+                    getline(cin, input);
+                    if (input.empty())
                     {
-                        break;
+                        if (x.pazymys.size() >= 1)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        int p = std::stoi(input);
+                        n++;
+                        x.pazymys.push_back(p);
+                        sum += p;
                     }
                 }
-                else
-                {
-                    int p = std::stoi(input);
-                    n += 1;
-                    x.pazymys.push_back(p);
-                    sum += p;
-                }
+                //-------------------------------------------------------------------------------------------
+                cout << "Iveskite egzamino rezultata:" << endl;
+                cin >> x.egzaminas;
             }
-            //-------------------------------------------------------------------------------------------
-            cout << "Iveskite egzamino rezultata:" << endl;
-            cin >> x.egzaminas;
-        }
-        x.vidurkis = (sum)/n;
+            x.vidurkis = (sum)/n;
 
-        //----------------------skaiciuojam mediana---------------------------
-        sort(x.pazymys.begin(), x.pazymys.end());
-        int dyd = x.pazymys.size();
-        if (dyd % 2 == 0)
-        {
-            x.mediana = static_cast<float>(x.pazymys[(dyd - 1) / 2] + x.pazymys[dyd / 2]) / 2;
+            //----------------------skaiciuojam mediana---------------------------
+            sort(x.pazymys.begin(), x.pazymys.end());
+            int dyd = x.pazymys.size();
+            if (dyd % 2 == 0)
+            {
+                x.mediana = static_cast<float>(x.pazymys[(dyd - 1) / 2] + x.pazymys[dyd / 2]) / 2;
+            }
+            else
+            {
+            x.mediana = x.pazymys[dyd / 2];
+            }
+            //---------------------------------------------------------------------
+            kursas.push_back(x);
+            x.pazymys.clear();
+            sum = 0;
+            n = 0;
         }
-        else
-        {
-        x.mediana = x.pazymys[dyd / 2];
-        }
-        //---------------------------------------------------------------------
-        kursas.push_back(x);
-        x.pazymys.clear();
-        sum = 0;
-        n = 0;
     }
+    else
+    {
+        cout << "Kiek namu darbu pazymiu?" << endl;
+        cin >> m;
+        Skaityk (m, x, kursas);
+    }
+
     cout << "Pagal ka skaiciuoti galutini bala? (1 - vidurkis, 2 - mediana)" << endl;
     cin >> y;
 
