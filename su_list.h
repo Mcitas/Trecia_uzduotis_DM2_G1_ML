024d81fc319fec79941446c05be346baef86b2a4
Mantas Laurutis <144427355+Mcitas@users.noreply.github.com>
2023-12-27 18:47:34 +0200

    1 STRATEGIJA header failai

---
+++ su_list.h
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <chrono>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+using std::end;
+using std::begin;
+using std::sort;
+using std::ifstream;
+using std::ofstream;
+using std::swap;
+using std::getline;
+using std::stoi;
+using std::cerr;
+using std::to_string;
+using std::list;
+
+struct Studentas {
+string vardas, pavarde;
+list <int> pazymys;
+int egzaminas;
+float vidurkis;
+float mediana;
+float balasm;
+float balasv;
+};
+
+int Skaiciaus_Ivedimas (int x1, int x2);
+void IrasykRanka (int m, Studentas &x, int &n, int &sum, int i);
+void Skaityk (list <Studentas> &kursas, int &m, string fpav);
+void VidurkisIrMediana (int &sum, int &n, Studentas &x, list <Studentas> &kursas);
+void Rikiuok (list <Studentas> &kursas);
+void Generuok (Studentas &x, int &n, int &sum, int loginis, int randomizer);
+void Failo_generavimas (int &m, int &n);
+void Isvedimas_i_konsole (list <Studentas> kursas);
+void Isvedimas_i_faila (list <Studentas> kursas, string pav);
+void Rusiuok (list <Studentas> &kursas, list <Studentas> &kietiakai, list <Studentas> &vargseliai);
---
+++ su_vector.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <chrono>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::vector;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+using std::end;
+using std::begin;
+using std::sort;
+using std::ifstream;
+using std::ofstream;
+using std::swap;
+using std::getline;
+using std::stoi;
+using std::cerr;
+using std::to_string;
+
+struct Studentas_v {
+string vardas, pavarde;
+vector <int> pazymys;
+int egzaminas;
+float vidurkis;
+float mediana;
+float balasm;
+float balasv;
+};
+
+int Skaiciaus_Ivedimas_v(int x1, int x2);
+void IrasykRanka_v(int m, Studentas_v &x, int &n, int &sum, int i);
+void Skaityk_v(vector<Studentas_v> &kursas, int &m, string fpav);
+void VidurkisIrMediana_v(int &sum, int &n, Studentas_v &x, vector<Studentas_v> &kursas);
+void Rikiuok_v(vector<Studentas_v> &kursas);
+void Generuok_v(Studentas_v &x, int &n, int &sum, int loginis, int randomizer);
+void Failo_generavimas_v(int &m, int &n);
+void Isvedimas_i_konsole_v(vector<Studentas_v> kursas);
+void Isvedimas_i_faila_v(vector<Studentas_v> kursas, string pav);
+void Rusiuok_v(vector<Studentas_v> &kursas, vector<Studentas_v> &kietiakai, vector<Studentas_v> &vargseliai);
+
